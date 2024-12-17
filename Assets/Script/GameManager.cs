770e1c4b223a4c1c8b5dad9ccccdf6fc02e1c397
ektor <>
2024-12-17 14:53:56 +0100

    he ido haciendo cosas no se cuales

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,14 +8,26 @@ public class GameManager : MonoBehaviour
     [SerializeField] public int puntuacionActual;
     [SerializeField] public int puntuacionMaxima;
     [SerializeField] public float tiempo;
+    [SerializeField] public GameObject gameOver, botonRestart;
+    [SerializeField] public GameObject jugador, enemigo;
+    [SerializeField] TMP_Text textoTiempo;
+    [SerializeField] public bool activarCronometro = true;
     // Start is called before the first frame update
-     void Start()
+    void Start()
     {
         puntuacionMaxima = PlayerPrefs.GetInt("puntuacionMaxima");
+        gameOver.SetActive(false);
+        botonRestart.SetActive(false);  
     }
      void Update()
     {
         tiempo += Time.deltaTime;
+        int minutos = (int)tiempo / 60;
+        int segundos = (int)tiempo % 60;
+        if (activarCronometro == true)
+        {
+            textoTiempo.text = $"{minutos:D2}: {segundos:D2}";
+        }
     }
     public void perder()
     {
